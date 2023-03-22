6b95a5d9b55ebab81f0f535a2daa0e1e0466aca9
Helly Prajapati <helly.p@simformsolutions.com>
2023-03-22 18:50:08 +0530

    TRaffic light changed.

---
+++ Traffic/script1.js
@@ -0,0 +1,13 @@
+// const fun = function () {
+//     const data = fetch("./index.html");
+//     return data;
+// }
+// console.log(fun())
+
+// const app = (function () {
+//   let count = 0;
+//   return function () {
+//     count++;
+//     console.log(count);
+//   };
+// })()();
